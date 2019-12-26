fb0dec591410d0c25c7613a7dd5603b89308fe30
swyin <swyin@thoughtworks.com>
2019-12-26 19:05:22 +0800

    refactor magic number

--- src/main/java/com/wuhantoc/javasample/utils/UnitConventer.java
+++ src/main/java/com/wuhantoc/javasample/utils/UnitConventer.java
@@ -2,9 +2,11 @@ package com.wuhantoc.javasample.utils;
 
 public class UnitConventer {
 
+  private static final double FOOT_TO_YARD_RATIO = 1d / 3d;
+
   public static double convertToYard(double number, String unit) {
     if ("foot".equals(unit)) {
-      return number / 3;
+      return number * FOOT_TO_YARD_RATIO;
     }
     return -1;
   }
