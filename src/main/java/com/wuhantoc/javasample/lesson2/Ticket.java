af8b1d4f1adfceb00dfbb2a6ae94fb7a7e9232a8
ylcai <ylcai@thoughtworks.com>
2020-01-07 16:32:16 +0800

    add test: pick successfully when given a correct ticket, and failed when given a wrong ticket

---
+++ src/main/java/com/wuhantoc/javasample/lesson2/Ticket.java
@@ -0,0 +1,4 @@
+package com.wuhantoc.javasample.lesson2;
+
+public class Ticket {
+}
--- src/test/java/com/wuhantoc/javasample/lesson2/LockerTest.java
+++ src/test/java/com/wuhantoc/javasample/lesson2/LockerTest.java
@@ -3,7 +3,6 @@ package com.wuhantoc.javasample.lesson2;
 import com.wuhantoc.javasample.lesson2.Locker;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import sun.security.krb5.internal.Ticket;
 
 public class LockerTest {
     @Test
@@ -33,6 +32,7 @@ public class LockerTest {
     @Test
     void should_get_package_when_pick_packages_given_have_correct_ticket() {
         // given
+        Locker locker = new Locker(1);
         Ticket correctTicket = locker.store();
 
         // when
@@ -40,6 +40,20 @@ public class LockerTest {
 
         // then
         Assertions.assertTrue(successful);
+    }
+
+    @Test
+    void should_get_package_when_pick_packages_given_have_wrong_ticket() {
+        // given
+        Locker locker = new Locker(1);
+        Ticket correctTicket = locker.store();
+
+        // when
+        Ticket wrongTicket = new Ticket();
+        boolean successful = locker.pick(wrongTicket);
+
+        // then
+        Assertions.assertFalse(successful);
     }
 
 }
