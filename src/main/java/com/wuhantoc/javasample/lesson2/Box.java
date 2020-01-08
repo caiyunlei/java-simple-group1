733ab96345d686a38d7420623b365595366d5b58
ylcai <ylcai@thoughtworks.com>
2020-01-08 18:21:00 +0800

    feature: add package to box

--- src/main/java/com/wuhantoc/javasample/lesson2/Box.java
+++ src/main/java/com/wuhantoc/javasample/lesson2/Box.java
@@ -4,6 +4,7 @@ public class Box {
   private boolean open = false;
   private boolean used = false;
   private final int id;
+  private Object somethingStored;
 
   public Box(int id) {
     this.id = id;
@@ -31,5 +32,13 @@ public class Box {
 
   public void close() {
     this.open = false;
+  }
+
+  public void putPackage(Object somethingToStore) {
+    this.somethingStored = somethingToStore;
+  }
+
+  public Object getPackage() {
+    return somethingStored;
   }
 }
