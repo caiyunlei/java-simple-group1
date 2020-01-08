d66d1890fe967fe338425b7ee2dde20000c6bcbf
ylcai <ylcai@thoughtworks.com>
2020-01-08 15:34:28 +0800

    rename test case

--- src/test/java/com/wuhantoc/javasample/lesson2/LockerTest.java
+++ src/test/java/com/wuhantoc/javasample/lesson2/LockerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class LockerTest {
     @Test
-    void should_get_ticket_when_store_packages_given_not_empty_box() {
+    void should_failed_when_store_package_given_0_boxes_locker() {
         // given
         Locker locker = new Locker(0);
 
@@ -17,22 +17,7 @@ public class LockerTest {
     }
 
     @Test
-    void should_get_ticket_when_store_packages_given_all_full_box() {
-        // given
-        Locker locker = new Locker(2);
-
-        Ticket ticket1 = locker.store();
-        Assertions.assertNotNull(ticket1);
-
-        Ticket ticket2 = locker.store();
-        Assertions.assertNotNull(ticket2);
-
-        Ticket ticket3 = locker.store();
-        Assertions.assertNull(ticket3);
-    }
-
-    @Test
-    void should_get_ticket_when_store_packages_given_empty_box() {
+    void should_get_ticket_when_store_package_given_have_empty_box_locker() {
         // given
         Locker locker = new Locker(1);
 
@@ -44,7 +29,21 @@ public class LockerTest {
     }
 
     @Test
-    void should_get_package_when_pick_packages_given_correct_ticket() {
+    void should_failed_when_store_package_given_all_full_box_locker() {
+        // given
+        Locker locker = new Locker(2);
+        locker.store();
+        locker.store();
+
+        //when
+        Ticket ticket3 = locker.store();
+
+        //then
+        Assertions.assertNull(ticket3);
+    }
+
+    @Test
+    void should_get_package_when_pick_given_correct_ticket() {
         // given
         Locker locker = new Locker(1);
         Ticket correctTicket = locker.store();
@@ -57,40 +56,44 @@ public class LockerTest {
     }
 
     @Test
-    void should_failed_when_pick_packages_given_wrong_ticket() {
+    void should_failed_when_pick_given_wrong_ticket() {
         // given
         Locker locker = new Locker(1);
         locker.store();
 
         // when
         Ticket wrongTicket = new Ticket();
-        boolean successful = locker.pick(wrongTicket);
+        boolean pickResult = locker.pick(wrongTicket);
 
         // then
-        Assertions.assertFalse(successful);
+        Assertions.assertFalse(pickResult);
     }
 
     @Test
-    void should_failed_when_pick_packages_given_used_ticket() {
+    void should_failed_when_pick_given_used_ticket() {
+        //given
         Locker locker = new Locker(1);
         Ticket ticket = locker.store();
+        locker.pick(ticket);
 
-        boolean pickResult = locker.pick(ticket);
-        Assertions.assertTrue(pickResult);
-
+        //when
         boolean repeatPickResult = locker.pick(ticket);
+
+        //then
         Assertions.assertFalse(repeatPickResult);
     }
 
     @Test
-    void should_success_when_store_package_given_pick_package_from_locker() {
+    void should_success_when_store_after_pick_given_locker() {
+        //given
         Locker locker = new Locker(1);
         Ticket ticket = locker.store();
+        locker.pick(ticket);
 
-        boolean pickResult = locker.pick(ticket);
-        Assertions.assertTrue(pickResult);
-
+        //when
         Ticket ticket1 = locker.store();
+
+        //then
         Assertions.assertNotNull(ticket1);
     }
 
